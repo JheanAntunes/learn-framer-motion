1653fb933c27952851880faa3edd7b1b64f8b1a7
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-13 18:54:22 -0300

    :sparkles:	feat: add data-links

---
+++ src/examples/menu-mobile/data-links.ts
@@ -0,0 +1,11 @@
+type DataLink = {
+  href: string
+  content: string
+}
+
+export const dataLinks: DataLink[] = [
+  { href: '#', content: 'Item1' },
+  { href: '#', content: 'Item2' },
+  { href: '#', content: 'Item3' },
+  { href: '#', content: 'Item4' }
+]
