c255758fa3136aecf001135b33774b9e4a70c55a
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-12 22:57:22 -0300

    :sparkles:	feat: add pattern 'use client' em components motions

--- src/components/framer-motion/index.tsx
+++ src/components/framer-motion/index.tsx
@@ -4,5 +4,6 @@ import { motion, Reorder } from 'framer-motion'
 
 export const MotionDiv = motion.div
 export const MotionLi = motion.li
+export const MotionUl = motion.ul
 export const ReorderGroup = Reorder.Group
 export const ReorderItem = Reorder.Item
