fde143ff683d3711479c46501aabce24f4f893e5
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-17 20:22:42 -0300

    :card_file_box:	raw: removendo um dado não desnecessario

--- src/examples/menu-mobile/data-links.ts
+++ src/examples/menu-mobile/data-links.ts
@@ -6,6 +6,5 @@ type DataLink = {
 export const dataLinks: DataLink[] = [
   { href: '/about', content: 'Sobre' },
   { href: '/contact', content: 'Contato' },
-  { href: '/project', content: 'Projeto' },
-  { href: '#', content: 'Item4' }
+  { href: '/project', content: 'Projeto' }
 ]
