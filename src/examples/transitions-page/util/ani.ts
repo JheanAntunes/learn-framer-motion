af07a78fe7e1a72d98520035c197cccf04443fcf
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-23 18:38:17 -0300

    :sparkles:	feat:  add fn util anin

---
+++ src/examples/transitions-page/util/ani.ts
@@ -0,0 +1,10 @@
+import { Variants } from 'framer-motion'
+
+export const anin = (variants: Variants) => {
+  return {
+    initial: 'initial',
+    animate: 'enter',
+    // exit: 'exit',
+    variants
+  }
+}
