8445acb1b898b5ce2c3c7fa838b6f710f3168cf6
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-23 18:35:10 -0300

    :sparkles:	feat:  add template for animation of transition page initial

---
+++ src/app/template.tsx
@@ -0,0 +1,8 @@
+import Inner from '@/examples/transitions-page/one-example/inner'
+import React from 'react'
+
+const template = ({ children }: React.PropsWithChildren) => {
+  return <Inner>{children}</Inner>
+}
+
+export default template
