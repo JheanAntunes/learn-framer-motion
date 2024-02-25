741670ec6b584023b98c893ca1cab174273dd3bc
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-24 21:34:07 -0300

    :sparkles:	feat: add dataLinks

---
+++ src/examples/awwwards/side-menu/nav/data-links.ts
@@ -0,0 +1,23 @@
+type DataLink = {
+  href: string
+  content: string
+}
+
+export const dataLinks: DataLink[] = [
+  {
+    href: '#',
+    content: 'Projects'
+  },
+  {
+    href: '#',
+    content: 'About'
+  },
+  {
+    href: '#',
+    content: 'Contact'
+  },
+  {
+    href: '#',
+    content: 'Agency'
+  }
+]
