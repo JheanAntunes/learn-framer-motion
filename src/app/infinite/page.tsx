db3c7e9429c8efaf7be82b1a5f09a8e91d7cd74f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-25 22:01:25 -0300

    :sparkles:	feat: add Component InfiniteTextMoveOnScroll example in UI infinite

---
+++ src/app/infinite/page.tsx
@@ -0,0 +1,11 @@
+import InfiniteTextMoveOnScroll from '@/examples/awwwards/Infinite-text-move-on-scroll'
+
+const PageInfinite = () => {
+  return (
+    <div className="overflow-hidden">
+      <InfiniteTextMoveOnScroll />
+    </div>
+  )
+}
+
+export default PageInfinite
