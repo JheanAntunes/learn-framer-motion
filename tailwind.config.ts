238902f74976fe323ae099864c572eefd108fd0a
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-04 19:53:01 -0300

    :sparkles:	feat: add plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,12 @@
 import type { Config } from 'tailwindcss'
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unused-vars
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const svgToDataUri = require('mini-svg-data-uri')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unused-vars
+const colors = require('tailwindcss/colors')
 const {
   default: flattenColorPalette
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -87,7 +95,32 @@ const config = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate'), addVariablesForColors]
+  plugins: [
+    require('tailwindcss-animate'),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: unknown) {
+      matchUtilities(
+        {
+          'bg-grid': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          }),
+          'bg-grid-small': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          }),
+          'bg-dot': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`
+          })
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      )
+    }
+  ]
 } satisfies Config
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
