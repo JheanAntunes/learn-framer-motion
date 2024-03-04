10cb300ed910ac470982dce996042731e8f54f7b
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-04 19:54:37 -0300

    :sparkles:	feat: add example horizontal scroll carousel with framer-motion

---
+++ src/app/horizontalscroll/page.tsx
@@ -0,0 +1,12 @@
+import MyHorizontalScrollCarousel from '@/components/horizontal-scroll-carousel.tsx/my-horizontal-scroll-carousel'
+
+const Page = () => {
+  return (
+    <div>
+      {/* <ExampleHorizontalScrollCarousel /> */}
+      <MyHorizontalScrollCarousel />
+    </div>
+  )
+}
+
+export default Page
