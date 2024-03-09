2a1fa6a4324f612f732449e99b831cb88b558a3f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-08 22:22:01 -0300

    :sparkles:	feat: add data inicial state cards

---
+++ src/app/notionkanban/data-card.tsx
@@ -0,0 +1,34 @@
+import { Cards } from './types'
+
+export const DEFAULT_CARD: Cards[] = [
+  //   BACKLOG
+  { title: 'Loook into render bug in dashboard', id: '1', column: 'backlog' },
+  { title: 'SOX compliance checklist', id: '2', column: 'backlog' },
+  { title: '[SPIKE] migrate to Azure', id: '3', column: 'backlog' },
+  { title: 'Document Notifications service', id: '4', column: 'backlog' },
+  //TODO
+  {
+    title: 'Reasearch DB options for new microservice',
+    id: '5',
+    column: 'todo'
+  },
+  { title: 'Postmortem for outage', id: '6', column: 'todo' },
+  { title: 'Sync with product on Q3 roadmap', id: '7', column: 'todo' },
+  //DOING
+  {
+    title: 'Refactor context providers to use Zustand',
+    id: '8',
+    column: 'doing'
+  },
+  {
+    title: 'Add loggin to daily CRON',
+    id: '9',
+    column: 'doing'
+  },
+  //DONE
+  {
+    title: 'Set up DD dashboards for lambda listener',
+    id: '10',
+    column: 'done'
+  }
+]
