8f19f8c378c1de0c15e047aa9f5127ea86459b0b
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-08 22:25:39 -0300

    :sparkles:	feat:add new page for NotionKanban project example

---
+++ src/app/notionkanban/page.tsx
@@ -0,0 +1,11 @@
+import { Board } from './board'
+
+const PageNotionKanban = () => {
+  return (
+    <div className="h-svh w-full bg-neutral-900 text-neutral-50">
+      <Board />
+    </div>
+  )
+}
+
+export default PageNotionKanban
