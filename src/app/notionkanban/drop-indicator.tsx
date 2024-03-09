8b0e70f6120a97812bc8942e23dc49dc0c2da456
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-08 22:21:39 -0300

    :sparkles:	feat: add component DropIndicator for drag card in columns

---
+++ src/app/notionkanban/drop-indicator.tsx
@@ -0,0 +1,18 @@
+import { Column } from './types'
+
+type PropsDropIndicator = {
+  beforeId: string
+  column: Column
+}
+export default function DropIndicator({
+  beforeId,
+  column
+}: PropsDropIndicator) {
+  return (
+    <div
+      data-before={beforeId || '-1'}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    ></div>
+  )
+}
