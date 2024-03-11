3dfeca6d8ecb54724a2eaa10da38e8abbb2be240
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-10 22:24:57 -0300

    :sparkles:	feat: add Component NotionRow

---
+++ src/app/notionwithzustand/components/notion-row.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils'
+const NotionRow = ({
+  children,
+  className,
+  ...restProps
+}: React.ComponentProps<'p'>) => {
+  return (
+    <div
+      draggable={true}
+      className={cn(
+        'my-5 cursor-grab rounded border border-solid border-neutral-800 p-5 text-neutral-200 active:cursor-grabbing',
+        className
+      )}
+      {...restProps}
+    >
+      <p className="text-neutral-200">{children}</p>
+    </div>
+  )
+}
+
+export default NotionRow
