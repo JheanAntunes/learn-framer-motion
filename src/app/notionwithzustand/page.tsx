a2b3612256519682b48ad336db2d6fe58b3095d5
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-03-10 22:23:54 -0300

    :sparkles:	feat: add Page para example do project NotionKanban com zustand e persistindo state no localstorage

---
+++ src/app/notionwithzustand/page.tsx
@@ -0,0 +1,11 @@
+import NotionTable from './components/notion-table'
+
+const Page = () => {
+  return (
+    <main className="bg-neutral-950">
+      <NotionTable />
+    </main>
+  )
+}
+
+export default Page
